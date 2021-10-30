dd46fe9c6f73fb68cf9102a8b0aef539285b40bf
limyounggyun <>
2021-10-30 22:28:28 +0900

    #16 DB와 WAS시간차이 발생 문제

--- src/main/java/com/electronicapproval/ElectronicApprovalApplication.java
+++ src/main/java/com/electronicapproval/ElectronicApprovalApplication.java
@@ -1,13 +1,23 @@
 package com.electronicapproval;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ElectronicApprovalApplication {
+	
+	// Server TimeZone Setting
+	@PostConstruct
+	void started() {
+//		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		// 한국 시간
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicApprovalApplication.class, args);
